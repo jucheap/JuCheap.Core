817af41fe7ac3d9e23e93be3ee946d58e4edc335
dj.wong <allenwdj@126.com>
2018-03-29 20:48:03 +0800

    api

---
+++ JuCheap.Core.WebApi/Controllers/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using JuCheap.Core.Interfaces;
+using JuCheap.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JuCheap.Core.WebApi.Controllers
+{
+    [Route("api/menu")]
+    public class MenuController : Controller
+    {
+        private readonly IMenuService _menuService;
+
+        public MenuController(IMenuService menuService)
+        {
+            _menuService = menuService;
+        }
+
+        [HttpGet]
+        [Route("search")]
+        public async Task<List<TreeDto>> GetMenus()
+        {
+            var menus = await _menuService.GetTreesAsync();
+            return menus;
+        }
+    }
+}
---
+++ JuCheap.Core.WebApi/Startup.cs
@@ -0,0 +1,70 @@
+﻿using JuCheap.Core.Data;
+using JuCheap.Core.Interfaces;
+using JuCheap.Core.Services;
+using JuCheap.Core.Services.AppServices;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace JuCheap.Core.WebApi
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            //使用Sql Server数据库
+            services.AddDbContext<JuCheapContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Connection_SqlServer")));
+            //支持sql2008的row_number分页函数
+            //services.AddDbContext<JuCheapContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Connection_SqlServer"), x => x.UseRowNumberForPaging()));
+
+            ////使用Sqlite数据库
+            //services.AddDbContext<JuCheapContext>(options => options.UseSqlite(Configuration.GetConnectionString("Connection_Sqlite")));
+
+            //使用MySql数据库
+            //services.AddDbContext<JuCheapContext>(options => options.UseMySql(Configuration.GetConnectionString("Connection_MySql")));
+
+            // Add application services.
+            // 1.automapper
+            services.AddScoped<AutoMapper.IConfigurationProvider>(_ => AutoMapperConfig.GetMapperConfiguration());
+            services.AddScoped(_ => AutoMapperConfig.GetMapperConfiguration().CreateMapper());
+
+            // 2.service
+            services.AddScoped<IDatabaseInitService, DatabaseInitService>();
+            services.AddScoped<ILogService, LogService>();
+            services.AddScoped<IPathCodeService, PathCodeService>();
+            services.AddScoped<IMenuService, MenuService>();
+            services.AddScoped<IRoleService, RoleService>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IDepartmentService, DepartmentService>();
+            services.AddScoped<IAreaService, AreaService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}

0e61345eec5ee7aaa2422d82e95ff0ca84a00bcb
dj.wong <allenwdj@126.com>
2018-05-10 10:52:46 +0800

    修复省市区和部门管理页面返回按钮无效的问题

---
+++ JuCheap.Core.Web/wwwroot/js/page/jucheap.action.js
@@ -0,0 +1,20 @@
+﻿(function () {
+	//返回按钮
+    $("#btnBack")
+        .bind("click",
+            function (e) {
+                var type = $(this).data("type");
+				if (type === "window") {//表示关闭弹框
+					parent.layer.closeAll();
+				}else if (type === "url") {//表示url跳转
+				    window.history.go(-1);
+				}
+            });
+    $("#btnSave")
+        .bind("click",
+            function() {
+                if ($("form:first").valid()) {
+                    $(this).button("loading");
+                }
+            });
+})();

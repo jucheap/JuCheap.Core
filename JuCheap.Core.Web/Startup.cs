6ec321e3eb47a53d39f505d0f707d81d37361fdb
dj.wong <allenwdj@126.com>
2018-06-25 21:43:04 +0800

    json日期格式化

--- JuCheap.Core.Web/Startup.cs
+++ JuCheap.Core.Web/Startup.cs
@@ -64,7 +64,10 @@ namespace JuCheap.Core.Web
             services.AddMvc(cfg =>
             {
                 cfg.Filters.Add(new RightFilter());
-            });            
+            }).AddJsonOptions(option =>
+            {
+                option.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
+            });
             //.AddJsonOptions(option => option.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver());//配置大小写问题，默认是首字母小写
 
             // service依赖注入
